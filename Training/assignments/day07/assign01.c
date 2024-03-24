fa7a6b8b6b90fef4f1b138a2d8db906acb3fe745
Kamlesh Lovewanshi <85796224+python-monster08@users.noreply.github.com>
2024-03-25 00:19:38 +0530

    Update assign01.c

--- Training/assignments/day07/assign01.c
+++ Training/assignments/day07/assign01.c
@@ -1,3 +1,34 @@
+/*
+    Q1. Write a program to find the  n-th number made of prime digits
+
+    Input Format:
+    First Line Of Input Contains T Number Of Test Cases
+    Second Line Of Input Contains an input Number N.
+    
+    Output Format:
+    Print the Nth number of sequence and it should be only prime.
+    
+    Constraints:
+    1<=T<=100
+    1<=N<=10000
+    
+    Examples :
+    Input  :
+    1
+    10
+    Output :
+     33
+           
+    Input  :
+    1
+    21
+    Output :
+     222
+
+*/
+
+
+
 #include <stdio.h>
 
 // Function to check if a number consists of prime digits only
